59f1ae71ac58508a1ec5e8ff7a1a6344c04ee52e
carlo <carlo.centrone@gerp.it>
2024-07-07 14:17:09 +0200

    add trim

--- src/Opengerp/Translation/Tools.php
+++ src/Opengerp/Translation/Tools.php
@@ -6,7 +6,11 @@ class Tools
 {
 
 
-    public static function fetchTermFromTemplateCode($file_content)
+    /**
+     * @param $file_content string
+     * @return array
+     */
+    public static function fetchTermsFromTemplateCode($file_content)
     {
         $string = $file_content;
 
@@ -19,5 +23,45 @@ class Tools
         $results = array_merge($results, $results2);
 
         return $results;
+    }
+
+    /**
+     * @param $file_content
+     * @param $vett_functions
+     * @return array
+     */
+    public static function fetchTermsFromCode($file_content, $vett_functions)
+    {
+        $string = $file_content;
+
+        $results = [];
+        foreach ($vett_functions as $function) {
+            $pattern = $function[0].'((.*?))(?=\))';
+            $index = $function[1];
+
+            preg_match_all("/$pattern/", $string, $addField);
+            foreach ($addField[1] as $key => $item) {
+                $arr_lems = explode(',', $item);
+                if (isset($arr_lems[$index])) {
+                    if (strpos($arr_lems[$index], 'trans(') == false) {
+                        $results[] = self::trimAll($arr_lems[$index]);
+                    }
+                }
+            }
+        }
+
+        return $results;
+
+
+    }
+
+    public static function trimAll($lemma)
+    {
+        $lemma = rtrim(trim($lemma),')');
+        $lemma = ltrim(trim($lemma),'(');
+        $lemma = trim(trim($lemma),'"');
+        $lemma = trim(trim($lemma),"'");
+
+        return $lemma;
     }
 }
